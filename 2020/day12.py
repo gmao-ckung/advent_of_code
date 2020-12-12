eb5e1a7cede7baba7b69cc6ac0aa1b84d88ad473
gmao-ckung <christopher.w.kung@nasa.gov>
2020-12-12 18:20:17 -0500

    Day 12 with part 2 solution

--- 2020/day12.py
+++ 2020/day12.py
@@ -14,6 +14,8 @@ NS_position = 0
 def rad(degrees):
     return degrees*math.pi/180
 
+# *** Part 1 ***
+
 for direction in directions:
     direction = direction.replace("\n","")
     action = direction[0]
@@ -44,3 +46,48 @@ for direction in directions:
 man_dist = abs(EW_position) + abs(NS_position)
 
 print("Part 1: Manhattan Distance =", man_dist)
+
+# *** Part 2 ***
+
+waypoint_EW = 10
+waypoint_NS = 1
+
+EW_position = 0
+NS_position = 0
+
+for direction in directions:
+    direction = direction.replace("\n","")
+    action = direction[0]
+    value = int(direction[1:])
+
+    if action == 'F':
+        EW_position = EW_position + waypoint_EW * value
+        NS_position = NS_position + waypoint_NS * value
+
+    elif action == 'N':
+        waypoint_NS = waypoint_NS + value
+
+    elif action == 'S':
+        waypoint_NS = waypoint_NS - value
+    
+    elif action == 'E':
+        waypoint_EW = waypoint_EW + value
+
+    elif action == 'W':
+        waypoint_EW = waypoint_EW - value
+
+    elif action == 'L':
+        new_waypoint_EW = waypoint_EW * math.cos(rad(-value)) + waypoint_NS * math.sin(rad(-value))
+        new_waypoint_NS = waypoint_EW * math.sin(rad(value)) + waypoint_NS * math.cos(rad(value))
+        waypoint_EW = new_waypoint_EW
+        waypoint_NS = new_waypoint_NS
+
+    elif action == 'R':
+        new_waypoint_EW = waypoint_EW * math.cos(rad(value)) + waypoint_NS * math.sin(rad(value))
+        new_waypoint_NS = waypoint_EW * math.sin(rad(-value)) + waypoint_NS * math.cos(rad(-value))
+        waypoint_EW = new_waypoint_EW
+        waypoint_NS = new_waypoint_NS
+
+man_dist = abs(EW_position) + abs(NS_position)
+
+print("Part 2: Manhattan Distance =", man_dist)
