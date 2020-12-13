1da1dd8b07bb7ac0a06d76fe7f87f08212791546
gmao-ckung <christopher.w.kung@nasa.gov>
2020-12-12 23:41:03 -0500

    Add Day 10 Part 2 solution

--- 2020/day10.py
+++ 2020/day10.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 print(CURR_DIR)
-joltage_list = np.sort(np.loadtxt(CURR_DIR+"/input.test",dtype=int))
+joltage_list = np.sort(np.loadtxt(CURR_DIR+"/input.day10",dtype=int))
 print(joltage_list)
 
 max_joltage = joltage_list[-1] + 3
@@ -23,3 +23,30 @@ joltage_difference[-1] = 3
 print("Count of difference of 1 =", np.count_nonzero(joltage_difference==1))
 print("Count of difference of 2 =", np.count_nonzero(joltage_difference==2))
 print("Count of difference of 3 =", np.count_nonzero(joltage_difference==3))
+
+# *** Part 2 ***
+flags = np.zeros(len(joltage_difference),dtype=int)
+for i in range(len(joltage_difference)):
+    if joltage_difference[i] == 1 and joltage_difference[i+1] == 3:
+        flags[i] = 1
+    if joltage_difference[i] == 3:
+        flags[i] = 1
+
+oneCount = 0
+numArragements = 1
+for flag in flags:
+    if flag == 0:
+        oneCount = oneCount + 1
+    elif flag == 1:
+        print(oneCount)
+        if oneCount == 1:
+            numArragements = numArragements * 2
+            oneCount = 0
+        elif oneCount == 2:
+            numArragements = numArragements * 4
+            oneCount = 0
+        elif oneCount == 3:
+            numArragements = numArragements * 7
+            oneCount = 0
+
+print("Number of Arrangements =", numArragements)   
