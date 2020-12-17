181fd6d8ec016b2fdd4032dc93e5fa20e5153fb7
gmao-ckung <christopher.w.kung@nasa.gov>
2020-12-17 12:49:41 -0500

    Day 16 Part 1 Solution validated

--- 2020/day16.py
+++ 2020/day16.py
@@ -2,20 +2,75 @@ import os
 import math
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 print(CURR_DIR)
-f1 = open(CURR_DIR+"/input.test")
+f1 = open(CURR_DIR+"/input.day16")
 data = f1.readlines()
+
+# *** Part 1 ***
+
+field_dict = {}
+ticket_dict = {}
 
 YT = False # Flag for "your ticket" info
 NT = False # Flag for "nearby tickets" info
 
+ticket_ID = 0
 for line in data:
     line=line.replace("\n","")
 
     if YT:
         your_ticket_numbers = [int(i) for i in line.split(",")]
+        ticket_dict[ticket_ID] = your_ticket_numbers
+        ticket_ID += 1
         YT = False
 
-    if line == "your ticket:":
+    elif NT:
+        nearby_ticket_numbers = [int(i) for i in line.split(",")]
+        ticket_dict[ticket_ID] = nearby_ticket_numbers
+        ticket_ID += 1
+
+    elif line == "your ticket:":
         YT = True
     elif line == "nearby tickets:":
         NT = True
+    elif line != "":
+        field_s = line.split(" ")
+        if(len(field_s) == 4):
+            field_type = field_s[0].replace(":","")
+            low_0 = int(field_s[1].split("-")[0])
+            high_0 = int(field_s[1].split("-")[1])
+            low_1 = int(field_s[3].split("-")[0])
+            high_1 = int(field_s[3].split("-")[1])
+        else:
+            field_type = field_s[0] + " " + field_s[1].replace(":","")
+            low_0 = int(field_s[2].split("-")[0])
+            high_0 = int(field_s[2].split("-")[1])
+            low_1 = int(field_s[4].split("-")[0])
+            high_1 = int(field_s[4].split("-")[1])
+        field_range = [low_0, high_0, low_1, high_1]
+        field_dict[field_type] = field_range
+
+invalid_check_dict = {}
+for nb_ticket in range(1,len(ticket_dict)):
+    nb_ticket_field_values = list(ticket_dict[nb_ticket])
+    for value in nb_ticket_field_values:
+        valid = False
+        for field_name in field_dict:
+            if (field_dict[field_name][0] > value) or \
+               (field_dict[field_name][1] < value and field_dict[field_name][2] > value) or \
+               (field_dict[field_name][3] < value):
+                valid |= False
+            else:
+                valid |= True
+                break
+    
+        if not valid:
+            invalid_check_dict[nb_ticket] = value
+            break
+
+#print(invalid_check_dict)
+
+error_rate = 0
+for i in invalid_check_dict.keys():
+    error_rate += invalid_check_dict[i]
+
+print("Ticket Scanning Error Rate=", error_rate)
