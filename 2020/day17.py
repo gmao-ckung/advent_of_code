6d24d352e1ca7c964add983040da81962f6c81f6
gmao-ckung <christopher.w.kung@nasa.gov>
2020-12-20 15:02:36 -0500

    Day 17 Part 2 Solution included

--- 2020/day17.py
+++ 2020/day17.py
@@ -12,6 +12,8 @@ z_len_init = 1
 
 total_cycles = 6
 
+# *** Part 1 ***
+
 # Initialize the potentially "active" cube region
 # based on the total number of cycles.  I also add
 # an extra boundary region layer to make the compute easier
@@ -52,7 +54,7 @@ def cell_update(x_index, y_index, z_index, curr_cell_domain, future_cell_domain)
         active = True
     
     neighbors_active = np.sum(curr_cell_domain[x_index-1:x_index+2,y_index-1:y_index+2,z_index-1:z_index+2]) \
-                     - curr_cell_domain[x_index][y_index][z_index]
+                     - curr_cell_domain[x_index,y_index,z_index]
     if active:
         if (neighbors_active == 2) or (neighbors_active == 3):
             future_cell_domain[x_index,y_index,z_index] = 1
@@ -73,4 +75,67 @@ for cycle in range(total_cycles):
     curr_cell_domain = np.copy(future_cell_domain)
 
 num_active_cells = np.sum(np.sum(curr_cell_domain))
-print("Number of Active Cells =", num_active_cells)
+print("Part 1: Number of Active Cells =", num_active_cells)
+
+# *** Part 2 ***
+
+w_len_init = 1
+total_w_len = w_len_init + (total_cycles + 1) * 2
+
+print("x-length =", total_x_len)
+print("y-length =", total_y_len)
+print("z-length =", total_z_len)
+print("w-length =", total_w_len)
+
+curr_cell_domain   = np.zeros((total_x_len,total_y_len,total_z_len,total_w_len),dtype=int)
+future_cell_domain = np.zeros((total_x_len,total_y_len,total_z_len,total_w_len),dtype=int)
+
+start_w_index = int((total_w_len - w_len_init) / 2)
+
+print("start_x_index =", start_x_index)
+print("start_y_index =", start_y_index)
+print("start_z_index =", start_z_index)
+print("start_w_index =", start_w_index)
+
+y_inc = 0
+for line in initial_cube:
+    line = line.replace("\n","")
+    x_inc = 0
+    for cell in line:
+        if cell == '#':
+            curr_cell_domain[start_x_index+x_inc,start_y_index+y_inc,start_z_index, start_w_index] = 1
+        x_inc += 1
+    y_inc += 1
+
+def cell_update_hyper(x_index, y_index, z_index, w_index, curr_cell_domain, future_cell_domain):
+    active = False
+    if curr_cell_domain[x_index,y_index,z_index,w_index] == 1:
+        active = True
+    
+    neighbors_active = np.sum(curr_cell_domain[x_index-1:x_index+2,
+                                               y_index-1:y_index+2,
+                                               z_index-1:z_index+2,
+                                               w_index-1:w_index+2]) \
+                     - curr_cell_domain[x_index,y_index,z_index,w_index]
+    if active:
+        if (neighbors_active == 2) or (neighbors_active == 3):
+            future_cell_domain[x_index,y_index,z_index,w_index] = 1
+        else:
+            future_cell_domain[x_index,y_index,z_index,w_index] = 0
+    else:
+        if neighbors_active == 3:
+            future_cell_domain[x_index,y_index,z_index,w_index] = 1
+        else:
+            future_cell_domain[x_index,y_index,z_index,w_index] = 0
+
+for cycle in range(total_cycles):
+    for i in range(1,total_x_len-1):
+        for j in range(1,total_y_len-1):
+            for k in range(1, total_z_len-1):
+                for w in range(1,total_w_len-1):
+                    cell_update_hyper(i,j,k,w,curr_cell_domain, future_cell_domain)
+
+    curr_cell_domain = np.copy(future_cell_domain)
+
+num_active_cells = np.sum(np.sum(np.sum(curr_cell_domain)))
+print("Part 2: Number of Active Cells =", num_active_cells)
