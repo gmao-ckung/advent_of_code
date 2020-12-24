cb3573e45f86c79cf5806f04d4169dcf84d7c402
gmao-ckung <christopher.w.kung@nasa.gov>
2020-12-24 11:06:03 -0500

    Day 18 Part 2 Solution

--- 2020/day18.py
+++ 2020/day18.py
@@ -62,14 +62,14 @@ def find_plus_paranthesis(curr_position, expression, pal_list):
         elif expression[curr_position] == '+':
             curr_position += 1
             if expression[curr_position] != '(':
-                pal_list.append(L_P_position)
-                pal_list.append(curr_position)
+                pal_list.append([L_P_position,'L'])
+                pal_list.append([curr_position,'R'])
                 curr_position += 1
             else:
                 curr_position += 1
-                pal_list.append(L_P_position)
+                pal_list.append([L_P_position,'L'])
                 p_result, curr_position = find_plus_paranthesis(curr_position,expression,pal_list)
-                pal_list.append(curr_position-1)              
+                pal_list.append([curr_position-1,'R'])
         elif expression[curr_position] == '-':
             curr_position += 1
             if expression[curr_position] != '(':
@@ -95,28 +95,21 @@ def find_plus_paranthesis(curr_position, expression, pal_list):
     return pal_list, curr_position
 
 def append_paranthesis(expression, list_of_paranthesis):
-    new_equation = []
-    type_par = 'R'
-    pal_index = len(list_of_paranthesis) - 1
-    for i in range(len(expression)-1,-1,-1):
-        #print(expression[i])
-        if list_of_paranthesis[pal_index] == i:
-            #print(type_par)
-            if type_par == 'L':
-                new_equation.append(expression[i])
-                new_equation.append('(')
-                type_par = 'R'
-            else:
-                new_equation.append(')')
-                new_equation.append(expression[i])
-                type_par = 'L'
-            pal_index -= 1
-        else:
-            new_equation.append(expression[i])
 
-    new_equation.reverse()
+    list_of_paranthesis.sort()
+    list_of_paranthesis.reverse()
+    #print(list_of_paranthesis)
 
-    return new_equation
+    for add_paran in list_of_paranthesis:
+        #print(add_paran)
+        p_type = add_paran[1]
+        p_location = add_paran[0]
+        if p_type == 'R':
+            expression = expression[:p_location+1] + ')' + expression[p_location+1:]
+        elif p_type == 'L':
+            expression = expression[:p_location] + '(' + expression[p_location:]
+    #print(expression)
+    return expression
 
 # *** Part 1 ***
 
@@ -125,7 +118,7 @@ total_sum = 0
 for expression in expressions:
     expression = expression.replace("\n","")
     expression = expression.replace(" ","")
-    print(expression)
+    #print(expression)
     curr_position = 0
     while(curr_position < len(expression)):
         result, curr_position = compute_expression(curr_position, expression)
@@ -135,18 +128,21 @@ print("Part 1: Sum of resulting expressions =", total_sum)
 
 # # *** Part 2 ***
 
-# total_sum = 0
+total_sum = 0
 
-# for expression in expressions:
-#     expression = expression.replace("\n","")
-#     expression = expression.replace(" ","")
-#     print(expression)
-#     curr_position = 0
-#     paranthesis_add_locations_list = []
-#     while(curr_position < len(expression)):
-#         paranthesis_add_locations_list, curr_position = find_plus_paranthesis(curr_position, expression, \
-#                                                                               paranthesis_add_locations_list)
-#     print(paranthesis_add_locations_list)
-#     new_equation = append_paranthesis(expression, paranthesis_add_locations_list)
-#     result, curr_position = compute_expression(0,new_equation)
-#     print('Sum =', result)
+for expression in expressions:
+    expression = expression.replace("\n","")
+    expression = expression.replace(" ","")
+    #print(expression)
+    curr_position = 0
+    paranthesis_add_locations_list = []
+    while(curr_position < len(expression)):
+        paranthesis_add_locations_list, curr_position = find_plus_paranthesis(curr_position, expression, \
+                                                                              paranthesis_add_locations_list)
+    #print(paranthesis_add_locations_list)
+    new_equation = append_paranthesis(expression, paranthesis_add_locations_list)
+    result, curr_position = compute_expression(0,new_equation)
+    #print('Sum =', result)
+    total_sum += result
+
+print("Part 2: Sum of resulting expressions =", total_sum)
