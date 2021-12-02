f4e01d3ce0b014a9604b330732ebf404658a3096
Chris Kung <christopher.w.kung@nasa.gov>
2021-12-01 19:17:34 -0500

    Started and verified Day 1 of Advent of Code 2021

---
+++ 2021/day1/day1.py
@@ -0,0 +1,41 @@
+import numpy as np
+import os
+CURR_DIR = os.path.dirname(os.path.realpath(__file__))
+print(CURR_DIR)
+data = np.loadtxt(CURR_DIR+'/input.day1')
+
+# *** Part 1 ***
+
+increase_count = 0
+
+for i in range(1,len(data)):
+    if data[i-1] < data[i]:
+        increase_count += 1
+
+print("Values increased", increase_count,  "times")
+
+# *** Part 2 ***
+
+increase_count = 0
+
+for i in range(len(data)):
+    window1_value = 0
+    window2_value = 0
+    # *** Window 1 ***
+    if i < len(data):
+        window1_value += data[i]
+    if (i+i) < len(data):
+        window1_value += data[i+1]
+        window2_value += data[i+1]
+    if (i+2) < len(data):
+        window1_value += data[i+2]
+        window2_value += data[i+2]
+    if (i+3) < len(data):
+        window2_value += data[i+3]
+
+    # print("window 1 value = ", window1_value)
+    # print("window 2 value = ", window2_value)
+    if window1_value < window2_value:
+        increase_count += 1
+
+print("There are", increase_count, "sums larger than the previous sum")
