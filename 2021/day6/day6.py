ad19d675427c90e39759d1b109ea933f00ed281b
Chris Kung <christopher.w.kung@nasa.gov>
2021-12-06 18:22:36 -0500

    Edited comments

--- 2021/day6/day6.py
+++ 2021/day6/day6.py
@@ -19,7 +19,7 @@ t1 = time.perf_counter()
 fishArray = iterateFishLife(fishArray, iterationDays)
 t2 = time.perf_counter()
 print("Part 1: Total Number of Fish = ", fishArray.shape[0])
-print("Version 1 runtime :", t2-t1)
+print("Part 1: Interative runtime :", t2-t1)
 
 fishArray = createFishArray(initialFishList)
 
@@ -31,7 +31,7 @@ for i in range(fishArray.shape[0]):
     totFish += numFish
 t2 = time.perf_counter()
 print("Part 1: Total Number of Fish = ", totFish)
-print("Version 2 runtime :", t2-t1)
+print("Part 1: Recursive runtime :", t2-t1)
 
 
 # *** "Memo" Implementation that improve recursion performance***
@@ -42,17 +42,17 @@ for i in range(fishArray.shape[0]):
     totFish += numFish
 t2 = time.perf_counter()
 print("Part 1: Total Number of Fish = ", totFish)
-print("Version 2 runtime :", t2-t1)
+print("Part 1: Recursive memo runtime :", t2-t1)
 
 # *** Part 2 ***
 
 totFish = 0
-
 fishBucket = np.zeros(6,dtype=np.int64)
 
-# Recusive code on Python is really slow for some reason, so I wrote up the
+# Recusive code, even with memo, on Python is really slow, so I wrote up the
 # recursive fish counter code in Fortran to get these "bucket" numbers that
 # can be used to compute the total number of fish
+
 fishBucket[0] = 6703087164
 fishBucket[1] = 6206821033
 fishBucket[2] = 5617089148
