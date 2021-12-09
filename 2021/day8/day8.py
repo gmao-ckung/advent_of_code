b99d67130ae2f69ce7aa1bd580e6ad3bbf16b2cc
Chris Kung <christopher.w.kung@nasa.gov>
2021-12-09 14:51:43 -0500

    Day 8 part 2 solution

--- 2021/day8/day8.py
+++ 2021/day8/day8.py
@@ -6,7 +6,7 @@ CURR_DIR = os.path.dirname(os.path.realpath(__file__))
 fopen = open(CURR_DIR+"/input.day8","r")
 initialDataList = fopen.readlines()
 
-outputDigits = grabOutputDigits(initialDataList)
+patternDigits, outputDigits = grabDigits(initialDataList)
 
 # print(outputDigits)
 
@@ -19,6 +19,67 @@ for output in outputDigits:
     numbers = output.split(" ")
     for number in numbers:
         if len(number) == 2 or len(number) == 4 or len(number) == 3 or len(number) == 7:
+            # print(number)
             count_1_4_7_8 += 1
 
 print("Part 1 : Total number of 1's, 4's, 7's, and 8's in output =", count_1_4_7_8)
+
+totalSum = 0
+
+for digit in range(len(patternDigits)):
+    pattern_dict = create_init_pattern_dict(patternDigits[digit],outputDigits[digit])
+
+    # Find "a" segment
+    # a_segment = 
+    a_segment = [x for x in set(sorted(list(pattern_dict[7]))).difference(set(sorted(list(pattern_dict[1]))))]
+    # Find "b" and "d" segments, but not know which one is which
+    bd_segments = [x for x in set(sorted(list(pattern_dict[4]))).difference(set(sorted(list(pattern_dict[1]))))]
+
+    # Find "g" segment by finding nine
+    patterns_len_6 = findPattern(patternDigits[digit],6)
+    for pattern in patterns_len_6:
+        if set(list(pattern_dict[4])).issubset(list(pattern)):
+            g_segment = [x for x in set(sorted(list(pattern))).difference(set(sorted(list(pattern_dict[4]) + a_segment)))]
+            pattern_dict[9] = pattern
+            break
+
+    # Find "e" segment by looking at eight
+    e_segment = [x for x in set(sorted(list(pattern_dict[8]))).difference(set(sorted(list(a_segment + list(pattern_dict[4]) + g_segment))))]
+
+    # Find zero and "d" segment
+    for pattern in patterns_len_6:
+        if set(list(pattern_dict[1]) + e_segment).issubset(list(pattern)):
+            pattern_dict[0] = pattern
+            d_segment = [x for x in set(sorted(list(pattern_dict[8]))).difference(set(sorted(list(pattern_dict[0]))))]
+
+    # Find six by process of elimination
+    patterns_len_6.remove(pattern_dict[9])
+    patterns_len_6.remove(pattern_dict[0])
+    pattern_dict[6] = patterns_len_6[0]
+
+    # find segments "c" and "f"
+    f_segment = [letter for letter in pattern_dict[1] if letter in pattern_dict[6]]
+    c_segment = [letter for letter in pattern_dict[1] if letter is not f_segment[0]]
+
+    patterns_len_5 = findPattern(patternDigits[digit], 5)
+
+    pattern_dict[2] = a_segment[0]+c_segment[0]+d_segment[0]+e_segment[0]+g_segment[0]
+    for pattern in patterns_len_5:
+        if set(list(pattern)).issubset(list(pattern_dict[6])):
+            pattern_dict[5] = pattern
+
+    pattern_dict[3] = a_segment[0] + c_segment[0] + d_segment[0] + f_segment[0] + g_segment[0]
+
+    number_str = ""
+    
+    for output in createList(outputDigits[digit]):
+        sorted_O_list = sorted(list(output))
+        for key in pattern_dict:
+            compare_list = sorted(list(pattern_dict[key]))
+            if compare_list == sorted_O_list:
+                number_str += str(key)
+                break
+            
+    totalSum += int(number_str)
+
+print("Part 2: Sum of Output values = ", totalSum)
--- 2021/day8/support.py
+++ 2021/day8/support.py
@@ -1,13 +1,39 @@
-def separateViaDelin(commaDelinLine, delin=","):
+def separateViaDelin(commaDelinLine, delin=",", part=-1):
     # Separate string via delineator
     lineListData = commaDelinLine.split(delin)
     lineListData[-1] = lineListData[-1].split("\n")[0]    # Get rid of the newline        
 
-    return lineListData[-1]
+    return lineListData[0], lineListData[1]
 
-def grabOutputDigits(inputList):
+def grabDigits(inputList):
+    patternDigits = []
     outputDigits = []
     for line in inputList:
-        outputData = separateViaDelin(line,"|")
+        patternData, outputData = separateViaDelin(line,"|")
+        patternDigits.append(patternData)
         outputDigits.append(outputData)
-    return outputDigits
+    return patternDigits, outputDigits
+
+def findPattern(input, numSeg):
+    return [x for x in input.split(" ") if len(x) == numSeg]
+
+def createList(input):
+    return [x for x in input.split(" ") if len(x) != 0]
+
+def create_init_pattern_dict(patternD, outputD):
+    one_pattern = findPattern(patternD, 2)
+    # if len(one_pattern) == 0:
+    #     one_pattern = findPattern(outputD, 2)
+    seven_pattern = findPattern(patternD, 3)
+    # if len(seven_pattern) == 0:
+    #     seven_pattern = findPattern(outputD, 3)
+    four_pattern = findPattern(patternD, 4)
+    # if len(four_pattern) == 0:
+    #     four_pattern = findPattern(outputD, 4)
+    eight_pattern = findPattern(patternD,7)
+    # if len(eight_pattern) == 0:
+    #     eight_pattern = findPattern(outputD,7)
+
+    pattern_dict = {1 : one_pattern[0], 7 : seven_pattern[0], 4 : four_pattern[0], 8: eight_pattern[0]}
+
+    return pattern_dict
