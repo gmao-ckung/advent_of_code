23585d589d9fb6be5ef4ed738664fca52bd8d376
Chris Kung <christopher.w.kung@nasa.gov>
2021-12-15 18:09:48 -0500

    Day 15 Part 2 Solution

--- 2021/day15/day15.py
+++ 2021/day15/day15.py
@@ -1,10 +1,7 @@
 import os
-# import numpy as np
-from queue import PriorityQueue
-from threading import current_thread
 
 CURR_DIR = os.path.dirname(os.path.realpath(__file__))
-fopen = open(CURR_DIR+"/input.test","r")
+fopen = open(CURR_DIR+"/input.day15","r")
 map = fopen.readlines()
 
 x_len = len(map[0].replace("\n",""))
@@ -14,14 +11,39 @@ frontier = []
 came_from = {}
 cost_so_far = {}
 
-frontier.append(['0,0',int(map[0].replace("\n","")[0])])
+def get_risk(x_coord, y_coord, map):
+    return int(map[y_coord].replace("\n","")[x_coord])
+
+def get_risk_v2(x_coord, y_coord, x_len, y_len, map):
+    scaled_x_coord = x_coord%x_len
+    scaled_y_coord = y_coord%y_len
+    tile_coord_x = int(x_coord/x_len)
+    tile_coord_y = int(y_coord/y_len)
+
+    starting_risk = int(map[scaled_y_coord].replace("\n","")[scaled_x_coord])
+    scaled_risk = starting_risk + tile_coord_x + tile_coord_y
+    if scaled_risk > 9:
+        scaled_risk -= 9
+    return scaled_risk
+
+def return_string_coord(x_coord, y_coord):
+    return str(x_coord)+","+str(y_coord)
+
+frontier.append(['0,0',0])
 came_from['0,0'] = None
-cost_so_far['0,0'] = int(map[0].replace("\n","")[0])
+cost_so_far['0,0'] = 0
 
 print(came_from)
 
+def sort_frontier_value(location):
+    return location[1]
+
+# Dijkstra Algorithm
 while len(frontier) > 0:
+    frontier.sort(reverse=True, key=sort_frontier_value)
     current_loc = frontier.pop()
+
+    print('current location = ', current_loc)
 
     if current_loc[0] == str(x_len-1)+','+str(y_len-1):
         break
@@ -29,4 +51,62 @@ while len(frontier) > 0:
     x_coord = int(current_loc[0].split(",")[0])
     y_coord = int(current_loc[0].split(",")[1])
 
-    print()
+    neighbors = [[x_coord-1,y_coord],
+                 [x_coord+1,y_coord],
+                 [x_coord,y_coord-1],
+                 [x_coord,y_coord+1]]
+
+    for neighbor in neighbors:
+        if neighbor[0] >= 0 and neighbor[0] < x_len and neighbor[1] >= 0 and neighbor[1] < y_len:
+            risk_value = get_risk_v2(neighbor[0], neighbor[1], x_len, y_len, map)
+            new_cost = cost_so_far[current_loc[0]] + risk_value
+
+            coord_string = return_string_coord(neighbor[0],neighbor[1])
+            if coord_string not in cost_so_far.keys() or new_cost < cost_so_far[coord_string]:
+                cost_so_far[coord_string] = new_cost
+                frontier.append([coord_string, new_cost])
+                came_from[coord_string] = current_loc[0]
+        
+print("Part 1 : Lower risk path total =", cost_so_far[str(x_len-1)+","+str(y_len-1)])
+
+frontier = []
+came_from = {}
+cost_so_far = {}
+
+frontier.append(['0,0',0])
+came_from['0,0'] = None
+cost_so_far['0,0'] = 0
+
+scaled_x_len = x_len*5
+scaled_y_len = y_len*5
+
+# Dijkstra Algorithm
+while len(frontier) > 0:
+    frontier.sort(reverse=True, key=sort_frontier_value)
+    current_loc = frontier.pop()
+
+    print('current location = ', current_loc)
+
+    if current_loc[0] == str(scaled_x_len-1)+','+str(scaled_y_len-1):
+        break
+
+    x_coord = int(current_loc[0].split(",")[0])
+    y_coord = int(current_loc[0].split(",")[1])
+
+    neighbors = [[x_coord-1,y_coord],
+                 [x_coord+1,y_coord],
+                 [x_coord,y_coord-1],
+                 [x_coord,y_coord+1]]
+
+    for neighbor in neighbors:
+        if neighbor[0] >= 0 and neighbor[0] < scaled_x_len and neighbor[1] >= 0 and neighbor[1] < scaled_y_len:
+            risk_value = get_risk_v2(neighbor[0], neighbor[1], x_len, y_len, map)
+            new_cost = cost_so_far[current_loc[0]] + risk_value
+
+            coord_string = return_string_coord(neighbor[0],neighbor[1])
+            if coord_string not in cost_so_far.keys() or new_cost < cost_so_far[coord_string]:
+                cost_so_far[coord_string] = new_cost
+                frontier.append([coord_string, new_cost])
+                came_from[coord_string] = current_loc[0]
+        
+print("Part 2 : Lower risk path total =", cost_so_far[str(scaled_x_len-1)+","+str(scaled_y_len-1)])
