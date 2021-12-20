f300868a6d0d334e686772bd9173f805038fdea1
gmao-ckung <christopher.w.kung@nasa.gov>
2021-12-19 21:33:54 -0500

    Update day 18 work

--- 2021/day18/day18.py
+++ 2021/day18/day18.py
@@ -15,3 +15,32 @@ for snailfish in snailfishes:
 
 print(snailfish_string)
 
+def find_snailfish_pair(snailfish_string, curr_index, level):
+    snailfish_pair = ""
+    while(snailfish_string[curr_index] != "]"):
+        if snailfish_string[curr_index] == "[":
+            level += 1
+            snailfish_pair += "["
+            snailfish_pair += find_snailfish_pair(snailfish_string, curr_index, level)
+        else:
+            snailfish_pair += snailfish_string[curr_index]
+        curr_index += 1
+
+    return snailfish_pair+"]"
+
+curr_index = 0
+reduced_snailfish = ""
+level = 0
+while curr_index < len(snailfish_string):
+    if snailfish_string[curr_index] == "[":
+        level += 1
+        reduced_snailfish += "["
+
+    elif snailfish_string[curr_index] == "]":
+        level -= 1
+        reduced_snailfish += "]"
+    else:
+        inner_pair = ""
+
+        while(snailfish_string[curr_index] != "]"):
+
