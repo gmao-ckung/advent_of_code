7c59eaac7a97bcc840a4e7863806b0526c35ee27
Christopher Kung <68066130+gmao-ckung@users.noreply.github.com>
2023-01-28 20:52:20 -0500

    Cleaned up solution for day 7

--- 2022/07/07.py
+++ 2022/07/07.py
@@ -8,9 +8,8 @@ upperDir = "None"
 currDir = "None"
 while(command_or_output):
     parsedCO = command_or_output.replace("\n","").split(" ")
-    print(parsedCO)
+    # print(parsedCO)
     if(parsedCO[0] == "$"):
-        # print("command found : ", parsedCO)
         if(parsedCO[1] == "cd"):
             if(parsedCO[2] == ".."):
                 currDir = fileSystem[currDir][".."]
@@ -25,7 +24,6 @@ while(command_or_output):
                     currDir = checkDir
                     fileSystem[currDir] = {}
                     fileSystem[currDir][".."] = upperDir
-                    # print(fileSystem[parsedCO[2]])
                 else:
                     currDir = fileSystem[checkDir]
 
@@ -35,10 +33,8 @@ while(command_or_output):
             commandFound = False
             while(command_or_output and not commandFound):
                 parsedCO = command_or_output.replace("\n","").split(" ")
-                print(parsedCO)
                 if(parsedCO[0] != "$"):
                     #Need to be able to account for multiple directories
-                    # print(parsedCO)
                     if(parsedCO[0] == 'dir'):
                         if 'dir' not in fileSystem[currDir].keys():
                             fileSystem[currDir][parsedCO[0]] = [parsedCO[1]]
@@ -48,29 +44,19 @@ while(command_or_output):
                         fileSystem[currDir][parsedCO[0]] = parsedCO[1]
                     command_or_output = fopen.readline()
                 else:
-                    # print(parsedCO)
                     commandFound = True
-                
-            # print(fileSystem[currDir])
-    
-dirSizes = {}
 
 def getDirSize(dir, fileSystem, upperDir):
     dirSize = 0
-    # for dir in directories:
     if upperDir == "":
         currDir = dir
     else:
         currDir = upperDir + "_" + dir
-    # print("dir", currDir,' contains the following: ', fileSystem[currDir])
     for data in fileSystem[currDir]:
-        # print(data)
         if data == 'dir':
-            # print("Going into directory ", fileSystem[currDir][data])
             for nextDir in fileSystem[currDir][data]:
                 dirSize += getDirSize(nextDir, fileSystem, currDir)
         elif data != '..':
-            # print("File size ", data, " found")
             dirSize += int(data)
     return dirSize
 
@@ -81,21 +67,21 @@ totalSum = 0
 for dir in fileSystem.keys():
     dirSize = 0
     dirSize += getDirSize(dir, fileSystem, "")
-    print("Size of directory ", dir, " = ", dirSize)
+    # print("Size of directory ", dir, " = ", dirSize)
     if dirSize <= 100000:
         totalSum += dirSize
 
 print("Total Sum = ", totalSum)
 
-# Par t2
+# Part 2
 
 freeSpace = 70000000 - getDirSize('/', fileSystem,"")
 minSpace = 70000000
 
 for dir in fileSystem.keys():
     dirSize = getDirSize(dir,fileSystem,"")
-    print("Size of directory ", dir, " = ", dirSize)
-    print("freeSpace + dirSize = ", freeSpace + dirSize)
+    # print("Size of directory ", dir, " = ", dirSize)
+    # print("freeSpace + dirSize = ", freeSpace + dirSize)
     if((minSpace > freeSpace + dirSize) & (30000000<(freeSpace + dirSize))):
         minSpace = freeSpace + dirSize
 
